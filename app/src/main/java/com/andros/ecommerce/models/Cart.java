621fd7d95224a43192c5d347c5765e5b1ef4b25e
androschen <androscc7@gmail.com>
2022-09-10 19:49:32 +0700

    add Cart model

---
+++ app/src/main/java/com/andros/ecommerce/models/Cart.java
@@ -0,0 +1,55 @@
+package com.andros.ecommerce.models;
+
+public class Cart {
+    int id, quantity;
+    Double price, totalPrice;
+    String title, image;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
