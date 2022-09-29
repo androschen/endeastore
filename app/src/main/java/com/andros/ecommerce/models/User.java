f3be70018678cf6f68667993a62d9397c77ad0c8
androschen <androscc7@gmail.com>
2022-09-29 15:09:53 +0700

    update User Model

--- app/src/main/java/com/andros/ecommerce/models/User.java
+++ app/src/main/java/com/andros/ecommerce/models/User.java
@@ -1,24 +1,23 @@
 package com.andros.ecommerce.models;
 
+import java.util.ArrayList;
+
 public class User {
-    public String images,name, email, password;
+    private String images,name, email, password;
+    private Double money;
+    private ArrayList<UserAddress> userAddresses;
 
     public User(){
 
     }
 
     public User(String name, String email, String password) {
+        this.money=0.0;
         this.images=null;
         this.name = name;
         this.email = email;
         this.password = password;
-    }
-
-    public User(String name, String email, String password,String images) {
-        this.images = images;
-        this.name = name;
-        this.email = email;
-        this.password = password;
+        this.userAddresses = new ArrayList<>();
     }
 
     public String getImages() {
@@ -51,5 +50,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    public ArrayList<UserAddress> getUserAddresses() {
+        return userAddresses;
+    }
+
+    public void setUserAddresses(ArrayList<UserAddress> userAddresses) {
+        this.userAddresses = userAddresses;
     }
 }
